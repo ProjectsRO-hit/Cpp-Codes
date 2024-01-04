649f9236b81c75f1c64771091eb1e231e68499b4
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2024-01-04 15:41:40 +0530

    Addin if else

---
+++ elseif.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // #ifndef ONLINE_JUDGE  // not part of code, used to beautify input and output
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+
+    int savings;
+    cin>>savings;
+
+    if(savings>5000) {
+        cout<<"Neha\n";
+    }else if (savings>2000) {
+        cout<<"Rashmi\n";
+    }
+    else {
+        cout<<"Friends\n";
+    }
+    return 0;
+}
