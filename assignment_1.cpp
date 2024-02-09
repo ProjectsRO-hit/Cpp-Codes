7d77c962b99c80180a5c08878071d56d0deed02c
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2024-02-09 20:17:37 +0530

    Adding assignment

---
+++ assignment_1.cpp
@@ -0,0 +1,29 @@
+/* Write a C++ program that asks which country the user lives in, lets the user type in the country and  and prints out a message
+
+I've heard great things about [Country]. I'd like to go there sometime.
+
+
+Example run :
+
+Program prints : Where do you live?
+
+User types : Finland
+
+Program prints : I've heard great things about Finland. I'd like to go sometime.
+
+*/
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+int main()
+{
+
+    string input;
+    cout << "Hey there! Where do you live?" << endl;
+    getline(cin, input); // fixing space issues for places/counties/continents which have more than 1 name! i.e. North America
+
+    cout << "Wow " << input << "! That's a really good place to live in, I would love to visit there!!" << endl;
+
+    return 0;
+}
