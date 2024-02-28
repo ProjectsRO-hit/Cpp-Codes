9ca8101d91b799503b70624138a9d7b5e3888e69
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2024-02-29 00:11:09 +0530

    modify test

--- test.cpp
+++ test.cpp
@@ -1,10 +1,16 @@
-#include<iostream>
-#include<bitset>
+#include <iostream>
+#include <bitset>
 
 using namespace std;
-int main(){
-    unsigned short xyz {60000}; // declaring short 
-    cout << bitset<16>(xyz)<<endl; //printing value in binary
-    
+int main()
+{
+    unsigned short xyz{60000};       // declaring short
+    cout << bitset<16>(xyz) << endl; // printing value in binary
+    cout << showbase << hex << xyz << endl;
+    cout << showbase << dec << xyz << endl;
+    cout << showbase << oct << xyz << endl;
+
+    cout << endl;
+
     return 0;
 }
