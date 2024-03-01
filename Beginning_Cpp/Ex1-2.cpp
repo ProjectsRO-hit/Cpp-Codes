54bf55611e5708f913e24c17dcdf85b309c03254
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2024-03-01 22:20:10 +0530

    01.03.2024

---
+++ Beginning_Cpp/Ex1-2.cpp
@@ -0,0 +1,13 @@
+// Exercise 1-2. Create and execute a program that outputs your name on one line and your age on the
+// next line. Define a variable to hold your age first.
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    int age {23};
+    cout << " Name: Rohit Kumar Singh" << endl;
+    cout << " Age: "<< age << endl;    
+
+    return 0;
+}
