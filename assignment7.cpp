26e6ada9be806d1c106e5132372352ded9956fa2
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2024-03-06 01:20:28 +0530

    changes

--- assignment7.cpp
+++ assignment7.cpp
@@ -1,5 +1,26 @@
-// There's a medical treatment that is only applicable if your age is between 21 and 39 inclusive (21 and 39 are included in the range) .
-//  Your task as a C++ developer is to write a program that checks whether the user is eligible for the treatment.
+// // There's a medical treatment that is only applicable if your age is between 21 and 39 inclusive (21 and 39 are included in the range) .
+// //  Your task as a C++ developer is to write a program that checks whether the user is eligible for the treatment.
+
+// #include <iostream>
+
+// using namespace std;
+// int main()
+// {
+//     int age;
+//     cout << "--Input age for eligibility--" << endl;
+//     cin >> age;
+
+//     if (age == 0 || age < 0)
+//     {
+//         cerr << "Invalid age! Please Try again!" << endl;
+//     }
+//     else
+//     {
+//         string s = (age >= 21 && age <= 39) ? "eligible" : "not eligible";
+//         cout << "The patient is " << s << " for Treatment" << endl;
+//     }
+//     return 0;
+// }
 
 #include <iostream>
 
@@ -10,14 +31,26 @@ int main()
     cout << "--Input age for eligibility--" << endl;
     cin >> age;
 
-    if (age == 0 || age < 0)
+    if (age <= 0)
     {
         cerr << "Invalid age! Please Try again!" << endl;
     }
     else
     {
-        string s = (age >= 21 && age <= 39) ? "eligible" : "not eligible";
-        cout << "The patient is " << s << " for Treatment" << endl;
+        if (age >= 21)
+        {
+            if (age <= 39)
+            {
+                string s = "eligible";
+                cout << "The patient is " << s << " for Treatment" << endl;
+            }
+        }
+        else
+        {
+            string s = "not eligible";
+            cout << "The patient is " << s << " for Treatment" << endl;
+        }
+
+        return 0;
     }
-    return 0;
 }
