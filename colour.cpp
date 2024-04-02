fce5542258c5487c9434db1f3ce340b5dfce22dd
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2024-04-02 20:36:11 +0530

    new codes

---
+++ colour.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+
+
+
+
+using namespace std;
+int main(){
+    cout << "\033[1;31mThis text is red. \033[0m" << endl;
+    cout << "\033[1;44mThis text has a blue background.\033[0m" << endl;
+    return 0;
+}
